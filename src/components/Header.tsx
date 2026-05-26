d05a3339567cd048c737adcc91f326060730b633
Frosti4 <stayfrosti4.2014@gmail.com>
2026-05-26 14:02:59 +0300

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Send, Phone } from 'lucide-react';
-import logo from '../assets/Logo.png.png';
+import logo from '../punlic/logo.png';
 
 interface HeaderProps {
   onNavItemClick?: (href: string) => void;
